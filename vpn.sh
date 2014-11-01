46597317832adf1be6d6805487da8c3714d17e29
omni360 <omni360@qq.com>
2014-11-01 22:01:00 +0800

    update

---
+++ vpn.sh
@@ -0,0 +1,15 @@
+#安装pptp 和config
+#sudo apt-get install pptp-linux pptpconfig
+#查看网络配置
+#ifconfig
+
+#route -n
+
+#ip route
+
+#创建GreenVPN
+sudo pptpsetup --create GreenVPN --server 198.13.107.85 --username omni360 --password xxxxxx --encrpt --start
+
+
+
+
